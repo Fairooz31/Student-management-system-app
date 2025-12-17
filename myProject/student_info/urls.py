3e16dfdb9287ade4f087a34a14caed580d74a919
Fairooz <21303004@iubat.edu>
2025-12-17 12:56:11 -0800

    This is my second commit

--- myProject/student_info/urls.py
+++ myProject/student_info/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add_student/', views.add_student, name = "add_student"),
+    path('show_student/', views.show_student, name = "show_student"),
+    path('delete_student/<int:id>', views.delete_student, name = "delete_student"),
+    path('view_student/<int:id>', views.view_student, name = "view_student"),
+    path('edit_student/', views.edit_student, name = "edit_student"),
+
 ]
